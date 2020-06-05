23d3e1a18a38a51d3501cec0be404c708642962b
saivignesh98 <dragoonvignesh1998@gmail.com>
2020-06-05 22:35:34 +0530

    Made veggies component and used CSS

---
+++ src/components/Functional/Veggies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../../css/BreadIngredients.module.css';
+
+const Veggies = () => {
+    return (
+        <div className={styles.veggies}>
+        </div>
+    )
+}
+
+export default Veggies;
--- src/components/StateContainers/Burger.js
+++ src/components/StateContainers/Burger.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import styles from '../../css/BreadIngredients.module.css';
 import BunTop from '../Functional/BunTop';
+import Veggies from '../Functional/Veggies';
 
 class Burger extends Component {
     render() {
         return (
             <div className={styles.burger}>
                 <BunTop />
+                <Veggies />
             </div>
         )
     }
