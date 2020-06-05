b2562e0e0929156559d39e899a0e097b23aab048
saivignesh98 <dragoonvignesh1998@gmail.com>
2020-06-05 21:27:19 +0530

    Changed folder structure and added Burger Builder component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Navbar } from 'reactstrap';
 import React, { Component } from 'react';
 import './App.css';
-import Layout from './components/Layout';
+import Layout from './components/Functional/Layout';
+import BurgerBuilder from './components/StateContainers/BurgerBuilder';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
+			<div>
 				<Layout>
-					<p>Test to see if layout works</p>
+					<BurgerBuilder/>
 				</Layout>
 			</div>
 		)
--- src/components/Functional/Layout.js
+++ src/components/Functional/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../../css/Layout.module.css';
 
 const Layout = (props) => (
     <React.Fragment>
         <div>Toolbar, SideDrawer, and Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </React.Fragment>
