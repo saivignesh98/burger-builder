5537a28e853cd8b7f1ea1649ba8804dd8125de4a
saivignesh98 <dragoonvignesh1998@gmail.com>
2020-06-06 20:16:17 +0530

    Reset to just bun top and bottom

--- src/components/StateContainers/Burger.js
+++ src/components/StateContainers/Burger.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import styles from '../../css/BreadIngredients.module.css';
 import BunTop from '../Functional/BunTop';
-import Veggies from '../Functional/Veggies';
 import BunBottom from '../Functional/BunBottom';
-import Meat from '../Functional/Meat';
-import Cheese from '../Functional/Cheese';
-import Patty from '../Functional/Patty'
 
 class Burger extends Component {
     render() {
         return (
             <div className={styles.burger}>
                 <BunTop />
-                <Veggies />
-                <Meat />
-                <Cheese />
-                <Patty />
                 <BunBottom />
             </div>
         )
